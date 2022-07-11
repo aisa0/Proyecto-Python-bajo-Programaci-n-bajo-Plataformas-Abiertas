14b328b41587c87ff958aa38d0cb288ca45aa1b8
isabel sabater <sabaterisabel2000@gmail.com>
2022-07-11 13:44:32 -0600

    Ultimas modificaciones

--- Battleship_Game/Proyecto_Battleship.py
+++ Battleship_Game/Proyecto_Battleship.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python3
 
+'''
+    -------BATTLESHIPS-------
+
+    Este programa muestra el juego Battleship en una interfaz grafica hecha en
+    Gtk glade
+
+    ¿Cómo funciona?
+   1. Se crea un tablero de 10x10 donde se van a posicionar las naves.
+   2. Dispone de 50 balas para hundir dichas naves.
+   3. Puede escoger con el mouse una fila y una columna para disparar. (Ej. B5)
+   4. Se indicará cada disparo errado.
+   5. Una nave no puede ser posicionada diagonalmente,si un disparo golpea
+   el resto de la nave está en alguno de los cuatro puntos cardinales.
+   6. Si las naves se hunden antes de usar todas las balas, usted gana.
+   De otra manera, usted pierde.
+
+   Legend:
+   1. 'O' = Parte de la nave golpeada con un disparo
+   2. '#' = Disparo errado
+
+   Integrantes del Grupo:
+   Ariel Mora Murcia - B24454
+   Isabel Sabater Aguilar -  B97037
+   Jose Carlos Acevedo Fontalvo - B90034
+'''
+
+
 # Se importan las librerias
 import gi
 from battleship_V2 import shoot_bullet
